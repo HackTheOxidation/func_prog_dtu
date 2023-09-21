2d693b12c82486cb10dc1bd395d69b88c1aa7406
HackTheOxidation <tomas.hagenau@protonmail.ch>
2023-09-21 23:45:23 +0200

    Began working on mini-project: Propositional Logic Solver.

---
+++ PropositionalLogic/LogicSolver/Library.fs
@@ -0,0 +1,25 @@
+﻿namespace LogicSolver
+
+module Propositional =
+
+    type Prop =
+        | A of string
+        | Dis of Prop * Prop
+        | Con of Prop * Prop
+        | Neg of Prop
+
+    let rec negationNormalForm = function
+        | Neg (Dis (l, r)) ->
+          Con (negationNormalForm (Neg l), negationNormalForm (Neg r))
+        | Neg (Con (l, r)) ->
+          Dis (negationNormalForm (Neg l), negationNormalForm (Neg r))
+        | Neg(Neg a) ->
+          negationNormalForm a
+        | a -> a
+
+    let rec disjuctiveNormalForm = function
+        | Con (a, Dis (b, c)) ->
+          Dis (disjuctiveNormalForm <| Con (a, b), disjuctiveNormalForm <| Con (a, c))
+        | Con (Dis (a, b), c) ->
+          Dis (disjuctiveNormalForm <| Con (a, c), disjuctiveNormalForm <| Con (b, c))
+        | a -> a
